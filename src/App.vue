c4a4b47d7aae3b1c888c4d776c339e18e8e28e48
mote2000 <honghai.chen@163.com>
2019-11-26 11:14:11 +0800

    第一次提交工作修改

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <breadcrumb></breadcrumb>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Breadcrumb from './components/Breadcrumb.vue'
+//import BlogPost from './components/BlogPost.vue'
+
 
 export default {
   name: 'app',
+  data : function () {
+    return {
+      posts: [
+        { id: 1, title: 'My journey with Vue' },
+        { id: 2, title: 'Blogging with Vue' },
+        { id: 3, title: 'Why Vue is so fun' }
+      ],
+      user: { 
+        name: 'chh',
+        password: 123456 
+      }
+    }
+  },
   components: {
-    HelloWorld
+    Breadcrumb
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
---
+++ src/components/UserPosts.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+   <p>Posts</p>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+    
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
