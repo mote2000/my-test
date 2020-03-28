5f3cb2c434cc43e51eee1054c84d2b15de951bdf
mote2000 <honghai.chen@163.com>
2020-03-28 15:53:43 +0800

    第三次提交

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,16 @@
     <!-- <router-link to="/">日报</router-link>
     <router-link to="/index">业务统计</router-link>
     <router-view /> -->
-    <dailyreport></dailyreport>
+    <consultdailyreport></consultdailyreport>
   </div>
 </template>
 
 <script>
-import dailyreport from './statistics/dailyreport/dailyreport'
+import consultdailyreport from './statistics/dailyreport/consultdailyreport'
 
 export default {
   components: {
-    dailyreport
+    consultdailyreport
   }
 }
 </script>
--- src/statistics/dailyreport/bdreport.vue
+++ src/statistics/dailyreport/bdreport.vue
@@ -1,6 +1,6 @@
 <template>
   <div id= "bdreport" style="padding-left: 19%;padding-top: 50px">
-    <el-table :data="tableData" border style="width: 80%">
+    <el-table :data="tableData" border triple height= "200" style="width: 80%">
     <el-table-column prop="serialNum" label="序号" width="50"></el-table-column>
     <el-table-column prop="name" label="姓名" width="180"></el-table-column>
     <el-table-column prop="company" label="公司" width="180"></el-table-column>
@@ -18,6 +18,36 @@
       return {
         tableData: [{
           serialNum: 1,
+          name: '王小虎',
+          company: '南京金茂',
+          department: '设计部',
+          position: '暖通工程师',
+          phone: '18652994529',
+          remarks: '经验不足，可以推荐主管'
+
+        },
+        {
+          serialNum: 2,
+          name: '王小虎',
+          company: '南京金茂',
+          department: '设计部',
+          position: '暖通工程师',
+          phone: '18652994529',
+          remarks: '经验不足，可以推荐主管'
+
+        },
+        {
+          serialNum: 3,
+          name: '王小虎',
+          company: '南京金茂',
+          department: '设计部',
+          position: '暖通工程师',
+          phone: '18652994529',
+          remarks: '经验不足，可以推荐主管'
+
+        },
+        {
+          serialNum: 4,
           name: '王小虎',
           company: '南京金茂',
           department: '设计部',
